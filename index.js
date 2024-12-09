7b24c094a127bbc1c2a86d5e1f5444c63b73dbb1
24LP2417 24LP2417 <24LP2417@admins-iMac.local>
2024-12-09 12:32:55 +0800

    function learned

--- index.js
+++ index.js
@@ -1,112 +1,183 @@
 // problem 1
-// let a=2, b, c;
-// b= 1;
-// c= 3;
+function niilber(a, b, c) {
+  let n = a + b + c;
+  return n;
+}
+// let xa = Number(prompt("a too")), xb = Number(prompt("a too")), xc = Number(prompt("a too"));
+// let hariu = niilber(xa,xb, xc)
+// console.log(hariu);
 
-//  console.log(a+b+c);
 //  Problem 2
-// let x= 1;
-// y=3*x-5;
-// console.log(y);
+function function1(x) {
+  let y = 3 * x - 5;
+  return y;
+}
 
 // last digit
-// let b= 476;
-// let y= b%10;
-// console.log(y)
+function lastdigit(b) {
+  let y = b % 10;
+  return y;
+}
 
 // 2 orontoi toonii tsupruudiin urjver
-// let a=27;
-// let x=a%10;
-// let y=(a-x)/10;
-// console.log(x*y)
+function digitplus(a) {
+  let x = a % 10;
+  let y = (a - x) / 10;
+  return x + y;
+}
 
 // 3 orontoi toonii tsipruudiin niilber
-// let x= 987;
-// let a= x%10;
-// let b=(x-a)/10%10;
-// let c=(x-a-b*10)/100;
-// console.log(a+b+c)
+function digitmultiply3(x) {
+  let a = x % 10;
+  let b = ((x - a) / 10) % 10;
+  let c = (x - a - b * 10) / 100;
+  return a * b * c;
+}
 
-// min sec
-// let x= 200;
-// let a= x%60;
-// let b=(x-a)/60
-// console.log(b)
-// console.log(a)
+// sec to min
+function secTomin(x) {
+  let a = x % 60;
+  let b = (x - a) / 60;
+  return [b, a];
+}
+// const hariu = secTomin(320);
+// console.log(hariu[0]+"-min", hariu[1]+"sec");
 
 // hour min sec
-// let x=7261
-// let a=x%3600
-// let b, d=a;
-// if(a>=60){
-//     b=(a-(d%10))/60
-// }
-// else{
-//     b=0
-// }
-// let c=(x-a)/3600
-// console.log(c)
-// console.log(b)
-// console.log(a)
+function secTohour(x) {
+  let c = Math.floor(x / 3600);
+  let b = Math.floor((x - c * 3600) / 60);
+  let a = (x - c * 3600) % 60;
+  return [c, b, a];
+}
+// const hariu = secTohour(8123);
+// console.log(hariu[0] + "-hour", hariu[1] + "min", hariu[2] + "-sec");
 
 // a toonii b zereg
-// let a=3, b=4, c=1
-// for (let i=0; i<b; i++){
-//     c=c*a
-// }
-// console.log(c)
+function degree(a, b) {
+  let c = 1;
+  for (let i = 0; i < b; i++) {
+    c = c * a;
+  }
+  return c;
+}
+// let a=Number(prompt("number")), b=Number(prompt("degree"));
+// const c=degree(a,b)
+// console.log(c);
 
 // Массивт байгаа эсэх
-// let A = [1, 2, 3, 4, 5]
-// let b=3, console=0
-// for(let i=0; i<5; i++){
-//     if(b==A[i]){
-//         console.log("Yes")
-//         c=1
-//         break
+function baigaaeseh(a=[], b){
+  let c=0;
+  for(let i=0; i<a.length; i++){
+    if(b==a[i]){
+        c=1;
+        break;
+    }
+  }
+  if(c===1) return "yes";
+  else return "no"
+}
+let A = [1, 2, 3, 4, 5]
+let b=3, console=0
+for(let i=0; i<5; i++){
+    if(b==A[i]){
+        console.log("Yes")
+        c=1
+        break
+    }
+  }
+// RGB7090 - Шагай
+// let k;
+// k=prompt();
+// let s=0;
+// for(let i=0; i<=k; i++){
+//   for(let j=0; j<=k; j++){
+//     for(let l=0; i+j+l<=k; l++){
+//       s++;
 //     }
-
+//   }
 // }
-// if(c==0){
-//     console.log("No")
-// }
-
-// let a = 4,
-//   s = 1;
-// for (let i = 1; i < a; i++) {
-//   s = s * i;
-//   console.log(i + "!=", s);
-// }
-// let n=8
-// let A = [1, 2, 3, 1, 1, 3, 1, 2]
-// let s=A[0]
-// for(let i=1; i<n; i++){
-//     for(let j=1; j<i; j++)
-//     if(A[j]+(A[j]+2)>=(A[j]+1)(A[j]+3)){
-
-//     }
-// }
+// console.log(s);
 
 //  RGB7603 - Супер жаал саад тойрон
-let n = 6;
-let k = 2;
-let A = [2, 4];
-let count = 0;
-let B = [];
-// for (let i = 0; i < n; i++){
-// for(let j=0; j<k; j++){
-//     if(i==A[k]){
-//     }
-// }
-// }
-// if(i+3==A[k]){
+//  let n, x, k;
+//   n=prompt();
+//   k=prompt();
+//   let a=[], b=[];
+//   a[0] = 1;
 
+//   for (let i = 0; i <= n; i++){
+//   b[n] = 0;
+//   }
+//   for (let i = 0; i < k; i++){
+//   x=prompt();
+//   b[x] = 1;
+//   }
+//   if (b[1] == 1)
+//   a[1] = 0;
+//   else
+//   a[1] = a[0];
+//   if (b[2] == 1)
+//   a[2] = 0;
+//   else
+//   a[2] = a[1] + a[0];
+//   for (let i = 3; i <= n; i++){
+//   if (b[i] == 1)
+//   a[i] = 0;
+//   else
+//   a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+//   }
+//   console.log(a[n]);
+
+// RGB7606 - Фибоначчийн n дахь гишүүн
+// let n;
+// n = prompt("n der utgaa ugnu uu");
+// let f = [];
+// f[1] = 1;
+// f[0] = 1;`
+// for (let i = 2; i < n; i++) {`
+//   f[i] = f[i - 1] + f[i - 2];
 // }
-for (let i = 1; i <= n; i++) {
-  if (i != A[count]) {
-    B.push(i);
-  } else {
-    count++;
-  }
-}
-console.log(B);
+// console.log(f[n - 1]);
+
+// RGB7702 - Бүжгийн хос
+// const quickSort = arr => {
+//   const a = [...arr];
+//   if (a.length < 2) return a;
+//   const pivotIndex = Math.floor(arr.length / 2);
+//   const pivot = a[pivotIndex];
+//   const [lo, hi] = a.reduce(
+//     (acc, val, i) => {
+//       if (val < pivot || (val === pivot && i != pivotIndex)) {
+//         acc[0].push(val);
+//       } else if (val > pivot) {
+//         acc[1].push(val);
+//       }
+//       return acc;
+//     },
+//     [[], []]
+//   );
+//   return [...quickSort(lo), pivot, ...quickSort(hi)];
+// };
+
+// let n,
+//   a = [];
+// for (let i = 0; i < n; i++) {
+//   a[0] = prompt("-r huvguunii ur chadvar");
+// }
+
+// let m,
+//   b = [];
+// for (let i = 0; i < n; i++) {
+//   b[0] = prompt(i + "-r ohinii ur chadvar");
+// }
+// let s = 0;
+// for (let i = 0; i < n; i++) {
+//   for (let j = i; j < n; j++) {
+//     if (a[i] == b[j] - 1 || a[i] == b[j] || a[i] == b[j] + 1) {
+//       s++;
+//       b[j] = -1;
+//     }
+//   }
+// }
+// console.log(a);
